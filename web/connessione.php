059fba8d0662c3c83329d4f02b4fb923a2ebc7b2
Fabio Sciortino <scio97@hotmail.it>
2019-12-23 19:56:35 +0100

    aggiornati dati accesso database

--- web/connessione.php
+++ web/connessione.php
@@ -1,13 +1,11 @@
 <?php
-$servername="localhost";
-$username="root";
-$password = "";
-$dbname= "dizionario";
+$server ="eu-cdbr-west-02.cleardb.net";
+$username = "b2f1387a4d8c19";
+$password = "08eb3522";
+$db= "heroku_cc602ece3a9cc08";
 
-$conn= new mysqli($servername, $username, $password, $dbname);
-
+$conn = new mysqli($server, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
